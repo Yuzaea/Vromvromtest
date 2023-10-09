b86b958db99b9b5ca5ae4a38b00da9533767aaee
Yuzaea <114402737+Yuzaea@users.noreply.github.com>
2023-10-09 23:59:44 +0100

    Add files via upload

---
+++ CarTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Carvromvroom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carvromvroom.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            var car = new Car { Model = "Ferraria" };
+            Assert.IsTrue(car.ValidateModel());
+        }
+        [TestMethod()]
+        public void ValidateInvaldModelTest()
+        {
+            var car = new Car { Model = "Fe" };
+            Assert.IsFalse(car.ValidateModel());
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            var car = new Car { Price = 7000 };
+            Assert.IsTrue(car.ValidatePrice());
+        }
+        [TestMethod()]
+        public void ValidateInvbalidPriceTest()
+        {
+            var car = new Car { Price = -7000 };
+            Assert.IsFalse(car.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+
+            var car = new Car { LicensePlate = "A14gf1" };
+            Assert.IsTrue(car.ValidateLicensePlate());
+
+        }
+        [TestMethod()]
+        public void ValidateINVALDLicensePlateTest()
+        {
+
+            var car = new Car { LicensePlate = "A1" };
+            Assert.IsFalse(car.ValidatePrice());
+
+        }
+    }
+}
